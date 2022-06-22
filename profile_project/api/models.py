9fe1efa5ac7d6dcd3423b800de9548ba211c7027
mdechavez5 <marcdechavez@gmail.com>
2022-06-22 14:40:37 -0400

    Setting up API

--- profile_project/api/models.py
+++ profile_project/api/models.py
@@ -1,3 +1,13 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=70)
+    repo = models.CharField(max_length=200)
+    live = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return title
--- profile_project/api/views.py
+++ profile_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import ProjectSerializer
+from .models import Project
 
 # Create your views here.
+
+class ProjectListView(generics.ListAPIView):
+    model = Project
+    serializer_class = ProjectSerializer
